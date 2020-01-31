ecdb433a8f4e6cce1ffe0d7fbdfa5cd111935702
Guido Amrein <guido.amrein@novabit.ch>
2020-01-31 16:37:31 +0100

    Color definition correction

--- src/qt/blockexplorer.cpp
+++ src/qt/blockexplorer.cpp
@@ -554,7 +554,7 @@ void BlockExplorer::setBlock(CBlockIndex* pBlock)
 
 void BlockExplorer::setContent(const std::string& Content)
 {
-    QString CSS = "body {font-size:12px; color:#ffffff; bgcolor:#008a00;}\n a, span { font-family: monospace; }\n span.addr {color:#008a00; font-weight: bold;}\n table tr td {padding: 3px; border: 1px solid black; background-color: #008a00;}\n td.d0 {font-weight: bold; color:#ffffff;}\n h2, h3 { white-space:nowrap; color:#008a00;}\n a { color:#88f6f6; text-decoration:none; }\n a.nav {color:#008a00;}\n";
+    QString CSS = "body {font-size:12px; color:#ffffff; bgcolor:#008a00;}\n a, span { font-family: monospace; }\n span.addr {color:#008a00; font-weight: bold;}\n table tr td {padding: 3px; border: 1px solid black; background-color: #008a00;}\n td.d0 {font-weight: bold; color:#ffffff;}\n h2, h3 { white-space:nowrap; color:#008a00;}\n a { color:#ffffff; text-decoration:none; }\n a.nav {color:#008a00;}\n";
     QString FullContent = "<html><head><style type=\"text/css\">" + CSS + "</style></head>" + "<body>" + Content.c_str() + "</body></html>";
     // printf(FullContent.toUtf8());
 
